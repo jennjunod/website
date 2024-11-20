27470aa7947168817174a7e187f43a435cde5d0b
Jenn Junod <jenn.junod@gmail.com>
2024-11-20 14:50:34 -0700

    seo updates

--- src/config.mjs
+++ src/config.mjs
@@ -2,7 +2,7 @@ export default {
     title: "Jenn Junod",
     titleSuffix: " | Full Stack Developer & Developer Advocate",
     description: "I specialize in making technology human — enabling developers to understand, adopt, and adore technical products.",
-    defaultOgImage: "jennog.jpg",
+    defaultOgImage: "https://opengraph.b-cdn.net/production/images/472241ee-3726-4492-b73b-976b50e8b0f1.jpg?token=33NiEpv5gmrrJBx8qHOX-pQ3Z9PA1Y7acKgzrGZXDRE&height=438&width=491&expires=33268139322",
         latestPosts: 3, 
     postsPerPage: 3,
     url: "https://jennjunod.dev",
