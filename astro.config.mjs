00b8b48ba5236d70b3ce5ffa0fc48a0a0918f9fe
Jenn Junod <jenn.junod@gmail.com>
2024-11-20 14:54:33 -0700

    trynig to replace seo image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://pacamara-astro-6y7xr.kinsta.page"
+  site: "https://opengraph.b-cdn.net/production/images/472241ee-3726-4492-b73b-976b50e8b0f1.jpg?token=33NiEpv5gmrrJBx8qHOX-pQ3Z9PA1Y7acKgzrGZXDRE&height=438&width=491&expires=33268139322"
 });
